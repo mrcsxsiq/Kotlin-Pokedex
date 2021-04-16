3c131a0c280fcc9697e0c6bf2180ffd742bf8d16
Vitor Hugo Risso da Silva <CIANDT\vitorhr@lnb028042cps.ciandt.global>
2021-04-16 10:28:59 -0300

    starting ui tests

---
+++ app/src/androidTest/java/dev/marcosfarias/pokedex/ui/dashboard/DashboardFragmentTest.kt
@@ -0,0 +1,57 @@
+package dev.marcosfarias.pokedex.ui.dashboard
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dev.marcosfarias.pokedex.R
+import dev.marcosfarias.pokedex.ui.home.HomeFragment
+import dev.marcosfarias.pokedex.ui.pokedex.PokedexFragment
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class DashboardFragmentTest {
+    lateinit var navController: TestNavHostController
+
+    @Before
+    fun setup() {
+// Create a TestNavHostController
+        navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+
+// Create a graphical FragmentScenario for the TitleScreen
+        launchFragmentInContainer(themeResId = R.style.AppTheme) {
+            PokedexFragment().also { fragment ->
+
+// In addition to returning a new instance of our Fragment,
+// get a callback whenever the fragment’s view is created
+// or destroyed so that we can set the NavController
+                fragment.viewLifecycleOwnerLiveData.observeForever { viewLifecycleOwner ->
+                    if (viewLifecycleOwner != null) {
+// The fragment’s view has just been created
+                        navController.setGraph(R.navigation.mobile_navigation)
+                        Navigation.setViewNavController(fragment.requireView(), navController)
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun checkIfPokemonInfoOpenWhenClickInAPokemon() {
+        Espresso.onView(
+            ViewMatchers.withId(R.id.recyclerView)
+        )
+    }
+}
